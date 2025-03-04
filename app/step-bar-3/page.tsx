cb6b119aec5af6e83b2eb494f3bca772d5c8efa8
bestdesignhub <your-email@example.com>
2025-03-04 17:14:19 +0530

    Add Website page design

---
+++ app/step-bar-3/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Stepbar() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/step-3.jpg"
+        alt="Home Page"
+      />
+    </div>
+  );
+}
