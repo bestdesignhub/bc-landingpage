29b98c715aac8cdb95565fb997fc27f6b1f3941f
bestdesignhub <your-email@example.com>
2025-03-05 16:06:35 +0530

    wom landing page

---
+++ app/social-duty-list-women/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/social-duty-list-women.jpg"
+        alt="Home Page"
+      />
+    </div>
+  );
+}
