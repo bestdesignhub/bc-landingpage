9a49b569acce5338eb76e647e43bee5762e7471a
bestdesignhub <your-email@example.com>
2025-03-05 15:38:17 +0530

    social duty page

---
+++ app/social-duty-home/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/social-duty-home.jpg"
+        alt="Home Page"
+      />
+    </div>
+  );
+}
