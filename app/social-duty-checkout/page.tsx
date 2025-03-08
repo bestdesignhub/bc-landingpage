6ee7c969779225bd96bbcaaabf9b8fa3513311a0
bestdesignhub <your-email@example.com>
2025-03-08 15:14:36 +0530

    Add pages

---
+++ app/social-duty-checkout/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/checkout-page.jpg"
+        alt="Checkout Page"
+      />
+    </div>
+  );
+}
