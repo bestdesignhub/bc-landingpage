9540aea246ce7b37a2b33babfd41a73d435a47e9
bestdesignhub <your-email@example.com>
2025-03-08 17:36:02 +0530

    add infographics

---
+++ app/social-duty-infographics/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/Social-Duty-Process.jpg"
+        alt="Thank you Page"
+      />
+    </div>
+  );
+}
