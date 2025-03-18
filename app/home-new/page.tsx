d01fa325403f61e45b3f389e0393870481299620
bestdesignhub <your-email@example.com>
2025-03-18 13:53:14 +0530

    add home page

---
+++ app/home-new/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/home-page-new.jpg"
+        alt="Home Page"
+      />
+    </div>
+  );
+}
