ee5b5d86aa1038402415030a6e753cd88dc373bc
bestdesignhub <your-email@example.com>
2025-03-24 13:07:06 +0530

    add new designs

---
+++ app/after-login-mesurment/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Stepbar() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/after-login-mesurment.jpg"
+        alt="Home Page"
+      />
+    </div>
+  );
+}
