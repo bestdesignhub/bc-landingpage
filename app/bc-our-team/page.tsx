a5523b7c11475f9369804422ffffa76d45d3c2de
bestdesignhub <your-email@example.com>
2025-03-25 10:13:07 +0530

    add pages

---
+++ app/bc-our-team/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Stepbar() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/our-team.jpg"
+        alt="Home Page"
+      />
+    </div>
+  );
+}
