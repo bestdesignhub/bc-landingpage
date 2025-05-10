5f405721f8fe84bb0cfcb7cafc12d6fbb6229901
unknown <macgw86@gmail.com>
2025-05-10 19:06:41 +0530

    add home page

---
+++ app/inventailor-home-small/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/Home-Page-small.jpg"
+        alt="Home Page"
+      />
+    </div>
+  );
+}
