50b96100564707ecb96c2b321e2483ba0a9ca713
unknown <macgw86@gmail.com>
2025-05-13 19:15:03 +0530

    add new pages

---
+++ app/inventailor-contact/page.tsx
@@ -0,0 +1,11 @@
+export default function InventailorContact() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/contact-inventailor.jpg"
+        alt="Design OM"
+      />
+    </div>
+  );
+}
---
+++ app/inventailor-design-om/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/design-om.jpg"
+        alt="Design OM"
+      />
+    </div>
+  );
+}
