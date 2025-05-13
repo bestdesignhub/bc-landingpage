c300032670dd748fd8d0711ebeb6e1c367c509fb
unknown <macgw86@gmail.com>
2025-05-13 15:18:40 +0530

    women page

---
+++ app/inventailor-women/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/women-page.jpg"
+        alt="Women Page"
+      />
+    </div>
+  );
+}
