dc0c264f8ba91bff1ce6f2c8849fd096bf0faeaf
unknown <macgw86@gmail.com>
2025-05-21 14:51:53 +0530

    add opage

---
+++ app/inventailor-shop/page.tsx
@@ -0,0 +1,11 @@
+export default function InventailorContact() {
+  return (
+    <div>
+       <img
+        className="w-full"
+        src="/Shop-Page.jpg"
+        alt="Design OM"
+      />
+    </div>
+  );
+}
